d8338f41419febc404b8628ac26a7971bbc3e3e7
BanafshaHotak <azmeenashah@yahoo.com>
2020-08-10 13:32:57 +0500

    quiz app coding

---
+++ question.js
@@ -0,0 +1,83 @@
+var questions = [{
+"question": "Which of the following is not JavaScript Data Types?",
+
+"option1": "Undefined",
+"option2": "Number",
+"option3": "Boolean",
+"option4": "Float",
+"answer": "4"
+},{
+    "question": "Which company developed JavaScript?",
+    
+    "option1": "Netscape",
+    "option2": "Bell Labs",
+    "option3": "Sun Microsystems",
+    "option4": "IBM",
+    "answer": "1"
+},{
+    "question": "Inside which HTML element do we put the JavaScript?",
+    
+    "option1": "<script>",
+    "option2": "<head>",
+    "option3": "<meta>",
+    "option4": "<style>",
+    "answer": "1"
+},{
+    "question": " Which of the following is correct about features of JavaScript?",
+    
+    "option1": "It can not Handling dates and time.",
+    "option2": "JavaScript is a object-based scripting language.",
+    "option3": "JavaScript is not interpreter based scripting language.",
+    "option4": "All of the above",
+    "answer": "2"
+},{
+    "question": "What is the original name of JavaScript?",
+    
+    "option1": " LiveScript",
+    "option2": " EScript",
+    "option3": "JavaScript",
+    "option4": "Mocha",
+    "answer": "4"
+},{
+    "question": "JavaScript is designed for following purpose ",
+    
+    "option1": "to style HTML pages",
+    "option2": "to execute Queries related to databases on a server",
+    "option3": "to add interactivity to html pages",
+    "option4": "All of the above",
+    "answer": "4"
+},{
+    "question": "Among the following, which one is a ternary operator in JavaScript?",
+    
+    "option1": "#",
+    "option2": "::",
+    "option3": "?:",
+    "option4": "&:",
+    "answer": "3"
+},{
+    "question": "Which of them is not the looping structures in JavaScript?",
+    
+    "option1": "for",
+    "option2": "While",
+    "option3": "forwhich",
+    "option4": "dowhile",
+    "answer": "3"
+},{
+    "question": "Among the keywords below, which one is not a statement?",
+    
+    "option1": "if",
+    "option2": "with",
+    "option3": "debugger",
+    "option4": "use strict",
+    "answer": "4"
+},{
+    "question": "What does javascript use instead of == and !=?",
+    
+    "option1": "It uses bitwise checking",
+    "option2": "It uses === and !== instead",
+    "option3": "It uses equals() and notequals() instead",
+    "option4": "It uses equal to()",
+    "answer": "2"
+},
+
+]
